5518ed4cb1ae433f3c189e0dae8ae1ae627e3ffd
eTrinityE <contaduduzi@gmail.com>
2020-05-15 19:49:06 -0300

    Leitura do volume

--- src/exercicio.java
+++ src/exercicio.java
@@ -6,10 +6,21 @@ public class exercicio {
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
 		
+		int qtdcompra;
+		double ticketm;
+		
 		//Cabecalho
 		System.out.println("SISTEMA DE PERFIL DE CLIENTE");
 		System.out.println("--------------------------------");
 		System.out.println("INFORME OS DADOS DO ULTIMO ANO");
+		
+		//Leitura do volume
+		System.out.printf("%n");
+		System.out.println("Quantas compras o cliente fez no ultimo ano?");
+		qtdcompra = sc.nextInt();
+		
+		System.out.println("Qual foi o ticket médio?");
+		ticketm = sc.nextDouble();
 		
 		sc.close();
 	}
