990180bfe8914354c2c8fc87ffefd0c0a8111830
eTrinityE <contaduduzi@gmail.com>
2020-05-15 19:39:26 -0300

    Cabecalho

--- src/exercicio.java
+++ src/exercicio.java
@@ -1,4 +1,17 @@
+import java.util.Locale;
+import java.util.Scanner;
 
 public class exercicio {
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		//Cabecalho
+		System.out.println("SISTEMA DE PERFIL DE CLIENTE");
+		System.out.println("--------------------------------");
+		System.out.println("INFORME OS DADOS DO ULTIMO ANO");
+		
+		sc.close();
+	}
 
 }
